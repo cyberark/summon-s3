8f40e17f7a94253ea4123e579c5371613d0ae180
Dustin Collins <dustinrcollins@gmail.com>
2015-06-07 21:17:29 -0400

    factor out error handling for consistency

--- main.go
+++ main.go
@@ -10,6 +10,23 @@ import (
 	"strings"
 )
 
+func handlePotentialError(err error, variableName string) {
+	if err != nil {
+		if awsErr, ok := err.(awserr.Error); ok {
+			var errMessage string
+			// A service error occurred
+			if reqErr, ok := err.(awserr.RequestFailure); ok {
+				errMessage = fmt.Sprintf("%v %v %v", reqErr.StatusCode(), reqErr.Message(), variableName)
+			} else {
+				errMessage = fmt.Sprintf("%v %v", awsErr.Code(), awsErr.Message())
+			}
+			printAndExit(errMessage)
+		} else {
+			printAndExit(err.Error())
+		}
+	}
+}
+
 func main() {
 	svc := s3.New(nil)
 	variableName := os.Args[1]
@@ -21,20 +38,7 @@ func main() {
 		Bucket: aws.String(bucketName),
 	}
 	_, err := svc.HeadBucket(params)
-	if err != nil {
-		if awsErr, ok := err.(awserr.Error); ok {
-			var errMessage string
-			// A service error occurred
-			if reqErr, ok := err.(awserr.RequestFailure); ok {
-				errMessage = fmt.Sprintf("%v %v %v", reqErr.StatusCode(), variableName, reqErr.Message())
-			} else {
-				errMessage = fmt.Sprintf("%v %v", awsErr.Code(), awsErr.Message())
-			}
-			printAndExit(errMessage)
-		} else {
-			printAndExit(err.Error())
-		}
-	}
+	handlePotentialError(err, variableName)
 
 	getParams := &s3.GetObjectInput{
 		Bucket: aws.String(bucketName),
@@ -42,15 +46,8 @@ func main() {
 	}
 
 	resp, err := svc.GetObject(getParams)
+	handlePotentialError(err, variableName)
 
-	if err != nil {
-		if awsErr, ok := err.(awserr.Error); ok {
-			errMessage := fmt.Sprintf("%s %s %s", awsErr.Code(), awsErr.Message(), variableName)
-			printAndExit(errMessage)
-		} else {
-			printAndExit(err.Error())
-		}
-	}
 	defer resp.Body.Close()
 	contents, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
