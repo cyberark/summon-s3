04e00736362c388ab27a0225d404acb6acaa2074
slimm609 <slimm609@gmail.com>
2017-11-21 15:20:58 -0500

    Fix sessions and add alpine binary creation for musl support (#3)
    * Fix session
    
    * Add alpine binary

--- build.sh
+++ build.sh
@@ -19,3 +19,13 @@ for GOOS in "${OSES[@]}"; do
     golang:1.9 \
       go build -v -o "$PKGDIR/$GOOS/summon-s3"
 done
+
+  echo "Building linux-alpine binary"
+  echo "-----"
+
+  docker run --rm \
+      -v "$PWD:/go/src/$APP" -w "/go/src/$APP" \
+      -e "GOOS=linux" -e "GOARCH=$GOARCH" \
+      golang:1.9-alpine \
+        go build -v -o "$PKGDIR/linux-alpine/summon-s3"
+
